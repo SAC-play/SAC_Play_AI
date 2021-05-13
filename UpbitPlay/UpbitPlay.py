095aa77862d16a8d6a1c192c80081560a37239a7
khw <hyunwook711@naver.com>
2021-05-13 16:38:12 +0900

    Strategy coding

--- UpbitPlay/UpbitPlay.py
+++ UpbitPlay/UpbitPlay.py
@@ -4,12 +4,48 @@
 import os
 import sys
 import json
-api_dirName=os.path.dirname(os.path.realpath(__file__))+'/../../Stock/Upbit/api'
+Current_path =os.path.dirname(os.path.realpath(__file__))
+api_dirName=Current_path+'/../../Stock/Upbit/api'
 sys.path.append(api_dirName)
 import UpbitApi as UA
 
 
 def do_work() :
-    with open(os.path.dirname(os.path.realpath(__file__))+'/strategy/ardr.json', 'r') as f:
+    #전략 불러오기
+    with open(Current_path+'/strategy/ardr.json', 'r') as f:
         json_data = json.load(f)
-    UA.get_market_info_ticker(json_data['market'])
+    coinName = json_data['market']
+    ceiling = int(json_data['ceiling'])
+    floor = int(json_data['floor'])
+    f.close
+
+    # 전략 코인 조회
+    json_ticker = UA.get_market_info_ticker(coinName)
+    for a in json_ticker:
+        tradePrice = int(a['trade_price'])
+
+    print(coinName + ' 현재 가격 : ' + str(tradePrice) + '원')
+
+    #계좌 내용 조회 
+    accountData = UA.get_my_account_info()
+    # "market" : "KRW-XRP",
+    # "ceiling" : "5",
+    # "floor" : "2"
+
+    #전략의 데이터가 계좌에 존재할 때, 사고 팔기
+    if any(coinName[coinName.find("-")+1:] in s['currency'] for s in accountData) :
+        for a in accountData:
+            if(a['currency']==coinName[coinName.find("-")+1:]):
+                avgBuyPrice=int(a['avg_buy_price'])
+                print('평단가 : ' + str(avgBuyPrice) +', 평단가+5% :' + str(avgBuyPrice+avgBuyPrice*(ceiling/100)))
+                if avgBuyPrice+avgBuyPrice*(ceiling/100) <= tradePrice :
+                    print('평단보다 5% 많아 판매를 진행합니다.') #판매함수 넣기
+                elif avgBuyPrice-avgBuyPrice*(floor/100) >= tradePrice :
+                    print('평단보다 2% 낮아 판매를 진행합니다.') #판매함수 넣기
+                else :
+                    print('어느 전략에도 도달하지 않았습니다.')
+    #전략의 데이터가 계좌에 존재하지 않을 때, 사고 팔기
+    else :
+        print(coinName + "은 계좌에 존재하지 않습니다.")
+        
+    
