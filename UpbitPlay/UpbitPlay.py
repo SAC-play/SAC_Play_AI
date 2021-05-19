a6426fc7ebbbe88bd51c0dcf33208b33b74df630
hyojinjdpark <hyojin.jd.park@gmail.com>
2021-05-19 14:14:29 +0900

    remove pycache directory

--- UpbitPlay/UpbitPlay.py
+++ UpbitPlay/UpbitPlay.py
@@ -8,7 +8,9 @@ Current_path =os.path.dirname(os.path.realpath(__file__))
 api_dirName=Current_path+'/../../Stock/Upbit/api'
 sys.path.append(api_dirName)
 import UpbitApi as UA
-
+Craw_api_dirName=Current_path+'/../../Crawling'
+sys.path.append(Craw_api_dirName)
+import CrawilngAPI as CRAW
 
 def do_work() :
     #전략 불러오기
@@ -25,14 +27,17 @@ def do_work() :
         tradePrice = int(a['trade_price'])
 
     print(coinName + ' 현재 가격 : ' + str(tradePrice) + '원')
-
+    # print(json_ticker)
     #계좌 내용 조회 
     accountData = UA.get_my_account_info()
     # "market" : "KRW-XRP",
     # "ceiling" : "5",
     # "floor" : "2"
-
-    #전략의 데이터가 계좌에 존재할 때, 사고 팔기
+    buy_sell_execute(coinName, accountData, ceiling, floor, tradePrice)
+    CRAW.crawling(accountData)
+# 사고 파는 함수   
+def buy_sell_execute(coinName, accountData, ceiling, floor, tradePrice) :
+    # 전략의 데이터가 계좌에 존재할 때,
     if any(coinName[coinName.find("-")+1:] in s['currency'] for s in accountData) :
         for a in accountData:
             if(a['currency']==coinName[coinName.find("-")+1:]):
@@ -47,5 +52,3 @@ def do_work() :
     #전략의 데이터가 계좌에 존재하지 않을 때, 사고 팔기
     else :
         print(coinName + "은 계좌에 존재하지 않습니다.")
-        
-    
